9e221aba6ef0c781301c2612695c946dcdc2d3fa
EduFStack <55258174+EduFStack@users.noreply.github.com>
2020-04-15 08:15:01 -0300

    criando o menu user

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -12,7 +12,7 @@
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/palette.css')}}">
-    <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon"/>
+    <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon" />
 </head>
 
 <body>
@@ -21,7 +21,7 @@
 
     <header class="container-fluid px-0 mx-0 fixed-top">
         <nav class="px-0 mx-0 navbar navbar-expand-lg color-nav-bar navbar-dark row justify-content-around pt-0 m-0">
-        <a class="navbar-brand col-auto" href="{{ route('home') }}">
+            <a class="navbar-brand col-auto" href="{{ route('home') }}">
                 <img class="header-logo" src="{{asset('img/logo/Coffee-logo-white.png')}}" alt="logo-cafe bay">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -30,48 +30,79 @@
             </button>
             <div class="collapse navbar-collapse row col-lg-8 justify-content-center justify-content-lg-end"
                 id="navbarNav">
-                <a href="{{ route('logout') }}"><button type="button" class="btn btn-light col-lg-auto col-11">
+                <div class="btn-group row col-lg-auto col-10 p-0 m-0">
+                    <button type="button" class="rounded-0 btn btn-light dropdown col-lg-12 col-12"
+                        data-toggle="dropdown">
                         <div class="row align-items-center justify-content-around m-0 p-0 col-lg-auto">
                             <img class="header-icon col-lg-auto pl-0" src="{{asset('img/Icons/user.svg')}}">
                             <h2 class="header-text col-lg-auto col-10 pl-0 mb-0">Olá, {{ Auth::user()->name }}</h2>
                         </div>
-                    </button></a>
-                <div class="col-lg-auto col-12 py-1"></div>
+                    </button>
+                    <div class="rounded-0 border border-dark dropdown-menu col-lg-12 col-12">
+                        <div class="row justify-content-center col-12 p-0 m-0">
+                        <button type="button" class="border -0 dropdown rounded-0 btn btn-outline-dark col-11 m-2">
+                            <div class="row col-12 align-items-center justify-content-between m-0 p-0">
+                                <img class="header-icon col-lg-2 col-auto p-0"
+                                    src="{{asset('img/Icons/carteira-de-identidade-wh.svg')}}">
+                                <h2 class="header-text col-auto pl-0 mb-0">Meus dados</h2>
+                            </div>
+                        </button>
+                        <button type="button" class="dropdown rounded-0 btn btn-outline-dark col-11 m-2">
+                            <div class="row col-12 align-items-center justify-content-between m-0 p-0">
+                                <img class="header-icon col-lg-2 col-auto p-0"
+                                    src="{{asset('img/Icons/carteira-de-identidade-wh.svg')}}">
+                                <h2 class="header-text col-auto pl-0 mb-0">Meus pedidos</h2>
+                            </div>
+                        </button>
+                        <button type="button" action="{{ route('logout') }}" class="dropdown rounded-0 btn btn-danger col-11 m-2">
+                            <div class="row col-12 align-items-center justify-content-between m-0 p-0">
+                                <img class="header-icon col-lg-2 col-auto p-0"
+                                    src="{{asset('img/Icons/carteira-de-identidade-wh.svg')}}">
+                                <h2 class="header-text col-auto pl-0 mb-0">Sair</h2>
+                            </div>
+                        </button>
+                        </div>
+                    </div>
+                </div>
+
+                <a href="{{ route('logout') }}"></a>
+                    <div class="col-lg-auto col-12 py-1"></div>
 
                 {{-- <button type="button" class=" btn btn-light dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <div class="row align-items-center justify-content-around m-0 p-0 col-lg-auto">
                         <img class="header-icon col-lg-auto col-auto pl-0"
                             src="{{asset('img/Icons/shopping-cart.svg')}}">
-                        <h2 class="header-text col-lg-auto col-auto col-10 pl-0 mb-0">CARRINHO</h2>
-                    </div>
-                </button>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <button class="dropdown-item col-auto btn btn-light" type="button">Ação</button>
-                    <button class="dropdown-item" type="button">Another Ação</button>
-                    <button class="dropdown-item" type="button">Algo mais aqui</button>
-                  </div>
+                <h2 class="header-text col-lg-auto col-auto col-10 pl-0 mb-0">CARRINHO</h2>
+            </div>
+            </button>
+            <div class="dropdown-menu dropdown-menu-right">
+                <button class="dropdown-item col-auto btn btn-light" type="button">Ação</button>
+                <button class="dropdown-item" type="button">Another Ação</button>
+                <button class="dropdown-item" type="button">Algo mais aqui</button>
+            </div>
             </div> --}}
 
-            <div class="btn-group">
-                <button type="button" class="btn btn-light dropdown" data-toggle="dropdown">
+            <div class="btn-group row col-lg-auto col-10 p-0 m-0">
+                <button type="button" class="rounded-0 btn btn-light dropdown col-lg-auto col-12"
+                    data-toggle="dropdown">
                     <div class="row align-items-center justify-content-around m-0 p-0 col-lg-auto">
                         <img class="header-icon col-lg-auto col-auto pl-0"
                             src="{{asset('img/Icons/shopping-cart.svg')}}">
-                        <h2 class="header-text col-lg-auto col-auto col-10 pl-0 mb-0">CARRINHO</h2>
+                        <h2 class="header-text col-auto pl-0 mb-0">CARRINHO</h2>
                     </div>
                 </button>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <button type="button" class="btn btn-light dropdown">
-                        <div class="row align-items-center justify-content-around m-0 p-0 col-lg-auto">
+                <div class="rounded-0 border border-dark dropdown-menu dropdown-menu-right">
+                    <button type="button" class="rounded-0 btn btn-warning dropdown">
+                        <div class="row align-items-center justify-content-around m-0 p-0">
                             <img class="header-icon col-lg-3 col-auto pl-0"
                                 src="{{asset('img/Icons/shopping-cart.svg')}}">
                             <h2 class="header-text col-lg-auto col-auto col-10 pl-0 mb-0">CARRINHO</h2>
                         </div>
                     </button>
-                  <button class="dropdown-item" type="button">teste </button>
-                  <button class="dropdown-item" type="button">Algo mais aqui</button>
+                    <button class="dropdown-item" type="button">teste </button>
+                    <button class="dropdown-item" type="button">Algo mais aqui</button>
                 </div>
-              </div>
+            </div>
 
         </nav>
     </header>
