db2ff070c449f742665fefdd0e435d2a028ef1eb
Eduardo Guilherme Camargo <egcamargo@EGSBR.local>
2020-04-19 11:02:24 -0300

    ainda na sacola

--- resources/views/sacola.blade.php
+++ resources/views/sacola.blade.php
@@ -6,6 +6,9 @@
 <br>
 <br>
 <br>
+
+{{-- ########### MODELO1 ########### --}}
+
 {{-- <section class="container-fluid row justify-content-center">
 
     <div class="row col-12 justify-content-lg-around">
@@ -51,42 +54,188 @@
 </section> --}}
 
 
+{{-- ########### MODELO2 ########### --}}
 
-<section class="container-fluid row justify-content-center">
-    <div class="row col-12 justify-content-center sacola-texto">
-        <h2>Itens da Campra</h2>
+{{-- <section class="container-fluid row p-0 m-0">
+    <div class="row col-lg-8 col-12 justify-content-around mx-4">
+        <div class="row col-10 pt-2 pb-4 m-0 sacola-texto align-items-end">
+            <h2 class="sacola-titulo p-0 m-0 rounded border-dark">Itens da Sacola</h2>
+        </div>
+        <div class=" row col-auto px-0 p-2 m-0 justify-content-end"> --}}
+
+
+
+{{-- <a class="produtos-link" href="">
+                <div class="card sacola-card m-1" style="width: 10rem">
+
+                    <img class="card-img-top m-0 p-0" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
+                    <div class="mt-0 mx-0 mb-2 p-0">
+                        <h5 class="sacola-titulo-card p-0 m-0">Ristreto</h5>
+                        <p class="sacola-subtitulo-card p-0 m-0">Intenso</p>
+                        <h5 class="sacola-qtd pt-2 pb-0 m-0">50</h5>
+                        <p class="sacola-texto2 text-center p-0 m-0">Saches</p>
+                        <div class="row col-12 justify-content-center p-2 m-0">
+                            <a class="btn btn-outline-danger col-4 border-danger" href=""><img class="sacola-icon"
+                                    src="{{asset('img/Icons/lixo.svg')}}" alt=""></a>
+</div>
+</div>
+
+</div>
+</a>
+<a class="produtos-link" href="">
+    <div class="card sacola-card m-1" style="width: 10rem">
+
+        <img class="card-img-top m-0 p-0" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
+        <div class="mt-0 mx-0 mb-2 p-0">
+            <h5 class="sacola-titulo-card p-0 m-0">Ristreto</h5>
+            <p class="sacola-subtitulo-card p-0 m-0">Intenso</p>
+            <h5 class="sacola-qtd pt-2 pb-0 m-0">50</h5>
+            <p class="sacola-texto2 text-center p-0 m-0">Saches</p>
+            <div class="row col-12 justify-content-center p-2 m-0">
+                <a class="btn btn-outline-danger col-4 border-danger" href=""><img class="sacola-icon"
+                        src="{{asset('img/Icons/lixo.svg')}}" alt=""></a>
+            </div>
+        </div>
+
     </div>
-    <div class=" row col-12 justify-content-center m-0 p-0">
+</a> --}}
+
+
+{{-- <a class="produtos-link" href="">
+                <div class="card sacola-card m-1" style="width: 11.2rem">
+
+                    <img class="card-img-top m-0 p-0" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
+                    <div class="mt-0 mx-0 mb-2 p-0">
+                        <h5 class="sacola-titulo-card p-0 m-0">Ristreto</h5>
+                        <p class="sacola-subtitulo-card p-0 m-0">Intenso</p>
+                        <h5 class="sacola-qtd pt-2 pb-0 m-0">50</h5>
+                        <p class="sacola-texto2 text-center p-0 m-0">Saches</p>
+                        <div class="row col-12 justify-content-center p-2 m-0">
+                            <a class="btn btn-outline-danger col-4 border-danger" href=""><img class="sacola-icon"
+                                    src="{{asset('img/Icons/lixo.svg')}}" alt=""></a>
+</div>
+</div>
+
+</div>
+</a>
+<a class="produtos-link" href="#">
+    <div class="card sacola-card-vazio m-1" style="width: 11.2rem">
+
+        <img class="card-img-top m-0 p-0" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
+        <div class="mt-0 mx-0 mb-2 p-0">
+            <h5 class="sacola-titulo-card p-0 m-0"></h5>
+            <p class="sacola-subtitulo-card p-0 m-0"></p>
+            <h5 class="sacola-qtd p-0 m-0"></h5>
+            <p class="sacola-texto2 text-center p-0 m-0"></p>
+            <div class="row col-12 justify-content-center p-2 m-0">
+                <a class="btn btn-outline-light col-4 border-dark" href=""><img class="sacola-icon"
+                        src="{{asset('img/Icons/lixo.svg')}}" alt=""></a>
+            </div>
+        </div>
+
+    </div>
+</a>
+<a class="produtos-link" href="#">
+    <div class="card sacola-card-vazio m-1" style="width: 11.2rem">
+
+        <img class="card-img-top m-0 p-0" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
+        <div class="mt-0 mx-0 mb-2 p-0">
+            <h5 class="sacola-titulo-card p-0 m-0"></h5>
+            <p class="sacola-subtitulo-card p-0 m-0"></p>
+            <h5 class="sacola-qtd p-0 m-0"></h5>
+            <p class="sacola-texto2 text-center p-0 m-0"></p>
+            <div class="row col-12 justify-content-center p-2 m-0">
+                <a class="btn btn-outline-light col-4 border-dark" href=""><img class="sacola-icon"
+                        src="{{asset('img/Icons/lixo.svg')}}" alt=""></a>
+            </div>
+        </div>
+
+    </div>
+</a>
+<a class="produtos-link" href="#">
+    <div class="card sacola-card-vazio m-1" style="width: 11.2rem">
+
+        <img class="card-img-top m-0 p-0" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
+        <div class="mt-0 mx-0 mb-2 p-0">
+            <h5 class="sacola-titulo-card p-0 m-0"></h5>
+            <p class="sacola-subtitulo-card p-0 m-0"></p>
+            <h5 class="sacola-qtd p-0 m-0"></h5>
+            <p class="sacola-texto2 text-center p-0 m-0"></p>
+            <div class="row col-12 justify-content-center p-2 m-0">
+                <a class="btn btn-outline-light col-4 border-dark" href=""><img class="sacola-icon"
+                        src="{{asset('img/Icons/lixo.svg')}}" alt=""></a>
+            </div>
+        </div>
+
+    </div>
+</a>
+
+
+</div>
+</div>
+<div class="row col-lg-auto col-0 border-right border-secondary m-0 p-0 ">
+</div>
+<div class="row col-lg-3 col-12 justify-content-center m-0 p-0">
+    <div class="row col-12 justify-content-center sacola-texto m-2">
+        <h2>Resumo do Pedido</h2>
+    </div>
+    <div class=" row col-auto justify-content-center p-2 sacola-resumo">
 
         <a class="produtos-link" href="">
-            <div class="card index-card" style="width: 10rem">
+            <div class="card index-card col-12 m-0 p-0">
 
                 <img class="card-img-top m-0 p-0" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
                 <div class="mt-0 mx-0 mb-2 p-0">
                     <h5 class="card-title sacola-titulo p-0 m-0">Ristreto</h5>
                     <p class="card-text sacola-subtitulo p-0 m-0">Intenso</p>
                     <h5 class="sacola-qtd p-0 m-0">50</h5>
-                    {{-- <div class="row col-12 m-0 p-0 justify-content-center"> --}}
-                        <p class="sacola-texto2 text-center p-0 m-0">Saches</p>
-                    {{-- </div> --}}
-                    {{-- <div class="row col-auto justify-content-center p-0 m-0"> --}}
-                        {{-- <div class="sacola-item col-auto p-1 m-0">
-                            <a class="btn btn-secondary" href=""><img class="sacola-icon"
-                                    src="{{asset('img/Icons/lapis.svg')}}" alt=""></a>
-                        </div> --}}
-                        <div class="sacola-item row col-12 justify-content-center p-0 m-0">
-                            <a class="btn btn-danger col-6" href=""><img class="sacola-icon"
-                                    src="{{asset('img/Icons/lixo.svg')}}" alt=""></a>
-                        </div>
-                    {{-- </div> --}}
+                    <p class="sacola-texto2 text-center p-0 m-0">Saches</p>
+                    <div class="sacola-item row col-12 justify-content-center p-0 m-0">
+                        <a class="btn btn-danger col-6 " href=""><img class="sacola-icon"
+                                src="{{asset('img/Icons/lixo.svg')}}" alt=""></a>
+                    </div>
                 </div>
 
             </div>
         </a>
+    </div>
+</div>
+
+</section> --}}
 
 
+
+<section class="container row col-lg-11 col-12 p-0 m-0 justify-content-center">
+    <div class="row col-auto justify-content-center m-0 p-0">
+        <div class="index-hover row col-12 justify-content-center m-0 p-0 border-bottom-0">
+            <div class="index-nhover card index-card mr-1 mt-1" style="width: 11.2rem">
+                <a class="produtos-link" href="">
+                    <img class="card-img-top" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
+                    <div class="card-body pt-1 px-3">
+                        <h5 class="card-title m-0 pb-1 index-titulo-produto">Ristretto Intenso</h5>
+                        <p class="card-text mb-0">INTENSO E LICOROSO</p>
+                        <br>
+                        <h5 class="card-title m-0 pb-2 index-intensidade-produto">Quantidade</h5>
+                        <div class="row justify-content-center align-items-center">
+                            <div class="border border-secondary rounded m-0 p-1" style="width: 4.5rem;">
+                                <h3 class="sacola-qtd-produto m-0 p-0 bg-transparent">25</h3>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="row col-12 mt-1 justify-content-center m-0 p-0 border-top-0 rounded-0">
+            <div class="card index-card py-0 m-0 mr-1" style="width: 11.2rem">
+                <button class="index-nhover btn btn-outline-danger col-12 border-danger">
+                    <img class="sacola-icon" src="{{asset('img/Icons/lixo.svg')}}" alt="">
+                </button>
+            </div>
+        </div>
+        
     </div>
 </section>
+
 
 
 @include('footer');
