d9293b78847b0b072f15108dd20b37290758e48f
EduFStack <55258174+EduFStack@users.noreply.github.com>
2020-04-20 08:34:59 -0300

    trabalhando na sacola

--- resources/views/sacola.blade.php
+++ resources/views/sacola.blade.php
@@ -205,36 +205,114 @@
 
 
 
-<section class="container row col-lg-11 col-12 p-0 m-0 justify-content-center">
-    <div class="row col-auto justify-content-center m-0 p-0">
-        <div class="index-hover row col-12 justify-content-center m-0 p-0 border-bottom-0">
-            <div class="index-nhover card index-card mr-1 mt-1" style="width: 11.2rem">
-                <a class="produtos-link" href="">
-                    <img class="card-img-top" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
-                    <div class="card-body pt-1 px-3">
-                        <h5 class="card-title m-0 pb-1 index-titulo-produto">Ristretto Intenso</h5>
-                        <p class="card-text mb-0">INTENSO E LICOROSO</p>
-                        <br>
-                        <h5 class="card-title m-0 pb-2 index-intensidade-produto">Quantidade</h5>
-                        <div class="row justify-content-center align-items-center">
-                            <div class="border border-secondary rounded m-0 p-1" style="width: 4.5rem;">
-                                <h3 class="sacola-qtd-produto m-0 p-0 bg-transparent">25</h3>
+<section class="container row col-12 pt-4 justify-content-around">
+    <div class="row col-8 justify-content-start">
+        <div class="col-12 p-0 pb-4 m-0 sacola-texto">
+            <h2 class="sacola-titulo p-0 m-0 rounded border-dark">Itens da Sacola</h2>
+        </div>
+        <div class="row col-12 justify-content-center p-3 m-0 rounded border border-dark">
+            <div class="index-hover row justify-content-center m-0 px-2 border-bottom-0">
+                <div class="index-nhover card index-card mr-1 mt-1" style="width: 11.2rem">
+                    <a class="produtos-link" href="">
+                        <img class="card-img-top" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
+                        <div class="card-body pt-1 px-3">
+                            <h5 class="card-title m-0 pb-1 index-titulo-produto">Ristretto Intenso</h5>
+                            <p class="card-text mb-0">INTENSO E LICOROSO</p>
+                            <br>
+                            <h5 class="card-title m-0 pb-2 index-intensidade-produto">Quantidade</h5>
+                            <div class="row justify-content-center align-items-center">
+                                <div class="border border-secondary rounded m-0 p-1" style="width: 4.5rem;">
+                                    <h3 class="sacola-qtd-produto m-0 p-0 bg-transparent">25</h3>
+                                </div>
                             </div>
                         </div>
+                    </a>
+                    <div class="row sacola-nhover col-auto justify-content-center m-0 mb-2 p-0">
+                        <button class="sacola-nhover btn btn-outline-danger col-8 border-danger p-1">
+                            <img class="sacola-icon" src="{{asset('img/Icons/lixo.svg')}}" alt="">
+                        </button>
                     </div>
-                </a>
+                </div>
+            </div>
+            <div class="index-hover row justify-content-center m-0 px-2 border-bottom-0">
+                <div class="index-nhover card index-card mr-1 mt-1" style="width: 11.2rem">
+                    <a class="produtos-link" href="">
+                        <img class="card-img-top" src="/img/Cards/Guatemala.webp" alt="Imagem de capa do card">
+                        <div class="card-body pt-1 px-3">
+                            <h5 class="card-title m-0 pb-1 index-titulo-produto">Ristretto Intenso</h5>
+                            <p class="card-text mb-0">INTENSO E LICOROSO</p>
+                            <br>
+                            <h5 class="card-title m-0 pb-2 index-intensidade-produto">Quantidade</h5>
+                            <div class="row justify-content-center align-items-center">
+                                <div class="border border-secondary rounded m-0 p-1" style="width: 4.5rem;">
+                                    <h3 class="sacola-qtd-produto m-0 p-0 bg-transparent">25</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <div class="row sacola-nhover col-auto justify-content-center m-0 mb-2 p-0">
+                        <button class="sacola-nhover btn btn-outline-danger col-8 border-danger p-1">
+                            <img class="sacola-icon" src="{{asset('img/Icons/lixo.svg')}}" alt="">
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="index-hover row justify-content-center m-0 px-2">
+                <div class="sacola-card-vazio card index-card mr-1 mt-1" style="width: 11.2rem">
+                    <a class="row col-12 produtos-link p-0 m-0 justify-content-center align-content-center" href="">
+                        <h5 class="col-12 card-title m-0 p-1 sacola-item">Adicionar novos itens</h5>
+                        <img class="col-6 sacola-icon-add p-3 m-4" src="{{asset('img/Icons/adicionar.svg')}}" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="index-hover row justify-content-center m-0 px-2">
+                <div class="sacola-card-vazio card index-card mr-1 mt-1" style="width: 11.2rem">
+                    <a class="row col-12 produtos-link p-0 m-0 justify-content-center align-content-center" href="">
+                        <h5 class="col-12 card-title m-0 p-1 sacola-item">Adicionar novos itens</h5>
+                        <img class="col-6 sacola-icon-add p-3 m-4" src="{{asset('img/Icons/adicionar.svg')}}" alt="">
+                    </a>
+                </div>
             </div>
         </div>
-        <div class="row col-12 mt-1 justify-content-center m-0 p-0 border-top-0 rounded-0">
-            <div class="card index-card py-0 m-0 mr-1" style="width: 11.2rem">
-                <button class="index-nhover btn btn-outline-danger col-12 border-danger">
-                    <img class="sacola-icon" src="{{asset('img/Icons/lixo.svg')}}" alt="">
-                </button>
+    </div>
+    <div class="row col-lg-auto col-0 border-right border-secondary m-0 p-0 "></div>
+    <div class=" col-lg-3 col-12 p-0 m-0 align-items-center">
+        <div class="row justify-content-center col-12 p-0 pb-4 m-0 sacola-texto">
+            <h2 class="sacola-titulo p-0 m-0 rounded border-dark">Resumo do Pedido</h2>
+        </div>
+        
+        <div class="row col-12 py-4 m-0 rounded sacola-resumo justify-content-center">
+            
+            <div class="row col-12 align-items-center justify-content-between p-0 m-0">
+                <h5 class="col-7 card-title m-1 px-0 sacola-item text-left sacola-resumo-texto">Ristretto Intenso</h5>
+                <h5 class="col-4 card-title m-1 px-0 sacola-item text-right sacola-resumo-texto">25 und.</h5>
+            </div>
+            <div class="row col-12 align-items-center justify-content-between p-0 m-0">
+                <h5 class="col-7 card-title m-1 px-0 sacola-item text-left sacola-resumo-texto">Ristretto Intenso</h5>
+                <h5 class="col-4 card-title m-1 px-0 sacola-item text-right sacola-resumo-texto">25 und.</h5>
+            </div>
+
+            <div class="row col-12 align-items-center justify-content-between p-0 pt-4 m-3">
+                <h5 class="col-7 card-title m-1 px-0 sacola-item text-center sacola-resumo-texto">TOTAL</h5>
+                <h5 class="col-4 card-title m-1 px-0 sacola-item text-right sacola-resumo-texto">50 und.</h5>
+            </div>
+
+            <div class="row pt-4 col-12 p-0 m-0 rounded justify-content-center">
+                <button class="btn btn-warning col-10 p-3 m-0 "> FINALIZAR PEDIDO </button>
+                <button class="btn  btn-outline-secondary col-10 p-0 m-0 mt-3">Cancelar Pedido</button>
+                
             </div>
         </div>
         
+        
+
+
+        
+            
+
     </div>
 </section>
+
 
 
 
